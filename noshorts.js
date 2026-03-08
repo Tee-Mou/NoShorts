9bb8bc87ad09755c44981c49004c392e56361e3b
Tee-Mou <tennysonmorris@outlook.com>
2026-03-08 23:01:14 +0000

    Updated to redirect to home page instead.

--- noshorts.js
+++ noshorts.js
@@ -1,5 +1,5 @@
-document.addEventListener('yt-navigate-finish', function(event) {
-    if (location.href.includes('/shorts/')) {
-        window.close()
+document.addEventListener(`yt-navigate-finish`, function() {
+    if (location.href.includes(`/shorts/`)) {
+        window.location.replace(`https://www.youtube.com/`)
     }
 });
